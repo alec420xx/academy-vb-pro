008d087b3596e0b136cb687d166696519207ca38
alec420xx <134131060+alec420xx@users.noreply.github.com>
2025-12-12 18:20:32 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2821
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red183\green111\blue247;\red23\green24\blue24;\red202\green202\blue202;
-\red212\green212\blue212;\red113\green192\blue131;\red109\green115\blue120;}
-{\*\expandedcolortbl;;\cssrgb\c77255\c54118\c97647;\cssrgb\c11765\c12157\c12549;\cssrgb\c83137\c83137\c83137;
-\cssrgb\c86275\c86275\c86275;\cssrgb\c50588\c78824\c58431;\cssrgb\c50196\c52549\c54510;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 import\cf4 \strokec4  \cf5 \strokec5 \{\cf4 \strokec4  defineConfig \cf5 \strokec5 \}\cf4 \strokec4  \cf2 \strokec2 from\cf4 \strokec4  \cf6 \strokec6 'vite'\cf4 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 import\cf4 \strokec4  react \cf2 \strokec2 from\cf4 \strokec4  \cf6 \strokec6 '@vitejs/plugin-react'\cf4 \cb1 \strokec4 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf7 \cb3 \strokec7 // https://vitejs.dev/config/\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2 export\cf4 \strokec4  \cf2 \strokec2 default\cf4 \strokec4  defineConfig\cf5 \strokec5 (\{\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \cb3   plugins\cf5 \strokec5 :\cf4 \strokec4  \cf5 \strokec5 [\cf4 \strokec4 react\cf5 \strokec5 ()],\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf5 \cb3 \strokec5 \})\cf4 \cb1 \strokec4 \
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
